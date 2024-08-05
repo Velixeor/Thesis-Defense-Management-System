6681a4230ea54c034007b571492e60a4f1cb5116
Maksim Skobaro <mkskobaro@gmail.com>
2024-08-05 21:45:56 +0300

    Migrate to modular project

--- server/src/main/java/ru/tubryansk/tdms/controller/TestController.java
+++ server/src/main/java/ru/tubryansk/tdms/controller/TestController.java
@@ -11,16 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @GetMapping
     public String root() {
-        return """
-                <!DOCTYPE html>
-                <html>
-                <head>
-                <title>Page Title</title>
-                </head>
-                <body>
-                <h1>My First Heading</h1>
-                </body>
-                </html>
-                """;
+        return "index.html";
     }
 }
